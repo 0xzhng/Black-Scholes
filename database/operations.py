d7454e9086807770d8561072930d3daf0eaa36d5
rynn-zhng <ryan@oslenlabs.com>
2025-03-16 00:00:00 -0400

    Add historical data tracking

--- database/operations.py
+++ database/operations.py
@@ -204,3 +204,5 @@ def get_snapshots_in_timerange(ticker_symbol, start_time, end_time):
 # Modified on 2025-03-13 00:00:00
 
 # Modified on 2025-03-15 00:00:00
+
+# Modified on 2025-03-16 00:00:00
--- utils/scheduler.py
+++ utils/scheduler.py
@@ -241,3 +241,5 @@ if __name__ == "__main__":
 # Modified on 2025-03-10 00:00:00
 
 # Modified on 2025-03-13 00:00:00
+
+# Modified on 2025-03-16 00:00:00
